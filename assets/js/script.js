99c0a2039e1b10c93be43b1688a5b78ac4b2ba28
kdingman <kcdonlan@yahoo.com>
2021-12-22 18:45:09 -0500

    touchups added

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,12 +55,8 @@ var auditTask = function(taskEl) {
     .text()
     .trim();
 
-    console.log(date);
-
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
-
-  console.log(time);
 
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -81,16 +77,18 @@ $(".card .list-group").sortable( {
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactive: function(event, ui) {
-    console.log("deactive", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", this);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", this);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event, ui) {
     // array to store the task data in
@@ -118,9 +116,6 @@ $(".card .list-group").sortable( {
     task[arrName] = tempArr;
     saveTasks();
   }
-  stop: function(event) {
-    $(this).removeClass("dropover");
-  }
 });
 
 // trash drop
@@ -129,12 +124,13 @@ $("#trash").droppable( {
   tolerance: "touch",
     drop: function(event, ui) {
       ui.draggable.remove();
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui) {
-      console.log("over");
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
-      console.log("out");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
@@ -156,7 +152,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -192,7 +188,6 @@ $(".list-group").on("blur", "textarea", function() {
   // get the textarea's current value/text
   var text = $(this)
     .val()
-    .trim();
 
   // get the parent's ul's id attribute
   var status = $(this)
@@ -290,3 +285,10 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+// audit task intervals
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (100 * 60) * 30);
